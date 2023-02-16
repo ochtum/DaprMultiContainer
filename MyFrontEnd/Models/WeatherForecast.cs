db04950487636c6798559dfc4244f2c19d9853e9
junichi takeda <j-takeda@sky-field.co.jp>
2023-02-16 18:21:19 +0900

    プロジェクト ファイルを追加します。

---
+++ MyFrontEnd/Models/WeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿namespace MyFrontEnd.Models
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF { get; set; }
+
+        public string Summary { get; set; } = string.Empty;
+    }
+}
---
+++ MyFrontEnd/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Dapr.Client;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyFrontEnd.Models;
+
+namespace MyFrontEnd.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DaprClient _daprClient;
+
+
+        public IndexModel(DaprClient daprClient)
+        {
+            _daprClient = daprClient;
+        }
+
+        public async Task OnGet()
+        {
+            var forecasts = await _daprClient.InvokeMethodAsync<IEnumerable<WeatherForecast>>(
+                HttpMethod.Get,
+                "MyBackEnd",
+                "weatherforecast");
+            ViewData["WeatherForecastData"] = forecasts;
+        }
+    }
+}
