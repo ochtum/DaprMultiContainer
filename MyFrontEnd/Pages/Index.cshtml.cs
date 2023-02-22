72a5d9471c8271408685d00d8e380da397f2e7c9
junichi takeda <j-takeda@sky-field.co.jp>
2023-02-22 15:32:33 +0900

    NewTraceデモ

--- MyFrontEnd/Pages/Index.cshtml.cs
+++ MyFrontEnd/Pages/Index.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using MyFrontEnd.Models;
 using System.Diagnostics;
 using Dapr.AspNetCore;
+using zipkin4net;
 
 namespace MyFrontEnd.Pages
 {
@@ -23,6 +24,19 @@ namespace MyFrontEnd.Pages
                 "MyBackEnd",
                 "weatherforecast");
             ViewData["WeatherForecastData"] = forecasts;
+            var trace = zipkin4net.Trace.Current.Child();
+            trace.Record(Annotations.ServerRecv());
+            trace.Record(Annotations.ServiceName("MyBackEnd1"));
+            trace.Record(Annotations.Rpc("GET1"));
+            trace.Record(Annotations.Tag("http.url", "http://zipkin:9411"));
+            trace.Record(Annotations.ServerSend());
+
+            var trace2 = zipkin4net.Trace.Create();
+            trace2.Record(Annotations.ServerRecv());
+            trace2.Record(Annotations.ServiceName("MyBackEndNew2222222222"));
+            trace2.Record(Annotations.Rpc("GET"));
+            trace2.Record(Annotations.Tag("http.url", "test"));
+            trace2.Record(Annotations.ServerSend());
 
         }
     }
